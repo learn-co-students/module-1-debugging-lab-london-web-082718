1b022ef4438cb843221a1f979cf8659ac08c3fac
klbennett <https://github.com/settings/applications>
2018-09-08 09:44:20 +0100

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,19 +1,21 @@
 
-
-def get_user_input
-  gets.chomp
-end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
+def get_user_input
+  choice = gets.chomp
+end
+
 def selection(num)
-  if num = 1
+
+  case num
+  when 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+     # binding.pry
+  when 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
